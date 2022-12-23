3936586bdcf3d60391bfb9a5217f839cc1692fea
KarthiS25 <karthis1015@gmail.com>
2022-12-23 11:15:40 +0530

    css changed

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,5 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: %i[ show edit update destroy ]
+  before_action :set_note, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
   def index
